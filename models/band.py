3038e56b4e9ea2c6787c46d9b48f446aea8662e1
Abiud Kiprotich <abiud.kiprotich@student.moringaschool.com>
2024-09-15 16:25:28 +0300

    imported __init__ for connection

--- models/band.py
+++ models/band.py
@@ -1,3 +1,5 @@
+from __init__ import CONN, CURSOR
+
 class Band:
     def __init__(self,name,hometown,id=None):
         self.id = id
--- models/venue.py
+++ models/venue.py
@@ -1,3 +1,4 @@
+from __init__ import CONN, CURSOR
 
 
 class Venue:
